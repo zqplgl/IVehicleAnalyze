820db682dd662cc05ccd4c81898764558e15d528
zqplgl <1316315878@qq.com>
2018-08-01 13:59:34 +0800

    add CMakeLists.txt

--- include/IVehicleAnalyze.h
+++ include/IVehicleAnalyze.h
@@ -1,42 +1,19 @@
 #ifndef IVEHICLEANALYZE_H_
 #define IVEHICLEANALYZE_H_
 
+
+#include <Itracker.h>
 namespace VehicleAnalyze
 {
-    #ifndef TRACKER
-    #define TRACKER
-
-    #include <string>
-    #include <vector>
-
-    using namespace std;
-
-    typedef vector<cv::Rect> Track;
-
-    struct Tracker
-    {
-        Track track;
-        size_t id;
-        int start_frame;
-        int end_frame;
-        int cls;
-    }
-
-    typedef vector<Tracker> Trackers;
-
-    #endif
-
     class IVehicleAnalyze
     {
         public:
-            virtual void input(const string &filePath)=0;
-            virtual void getTracks(Trackers &trackers)=0;
+            virtual void input(const string &filepath)=0;
+            virtual void getTracks(vector<tracker::Tracker> &trackers)=0;
             virtual ~IVehicleAnalyze(){}
-
     };
 
     IVehicleAnalyze *CreateIVehicleAnalyze(const string &model_dir,const int gpu_id,const int frameskip, const int numnull);
-    void DestroyIVehicleAnalyze(IVehicleAnalyze *ptr);
 }
 
 #endif
--- src/VehicleAnalyze.cpp
+++ src/VehicleAnalyze.cpp
@@ -1,51 +1,85 @@
+#include <opencv2/opencv.hpp>
+
 #include <IVehicleAnalyze.h>
-#include <IObjZoneDetectYoloV3.h>
+#include <IObjZoneDetect.h>
 
 namespace VehicleAnalyze
 {
-    using namespace ZoneDetectYoloV3;
+    using namespace ObjZoneDetect;
+    using namespace cv;
 
     class VehicleAnalyze: public IVehicleAnalyze
     {
         public:
-            VehicleAnalyze(const &string mode_dir,const int gpu_id, const int frameskip,const int numnull);
-            virtual void input(const string &filePath);
-            virtual void getTracks(Trackers &trackers);
+            VehicleAnalyze(const string& model_dir,const int gpu_id, const int frameskip,const int numnull);
+            virtual void input(const string &filepath);
+            virtual void getTracks(vector<tracker::Tracker> &trackers);
 
         private:
-            IObjZoneDetectYoloV3 *detector=nullptr;
+            IObjZoneDetect *detector=nullptr;
             int frameskip=0;
             int numnull=0;
             int gpu_id = 0;
+            VideoCapture capture;
+            int capindex = 0;
+            float confidence_threshold = 0;
     };
 
-    VehicleAnalyze::VehicleAnalyze(const &string mode_dir,const int gpu_id, const int freamskip,const int numnull):frameskip(frameskip),numnull(numnull),gpu_id(gpu_id)
+    VehicleAnalyze::VehicleAnalyze(const string& model_dir,const int gpu_id, const int freamskip,const int numnull):frameskip(frameskip),numnull(numnull),gpu_id(gpu_id)
     {
-        detector = CreateObjZoneYoloV3Detector();
+        string temp_model_dir = "";
+        if(model_dir[model_dir.size()-1]=='/')
+            temp_model_dir = model_dir;
+        else
+            temp_model_dir = model_dir + "/";
+
+
+        detector = CreateObjZoneYoloV3Detector(temp_model_dir+"yolov3/yolov3.cfg",temp_model_dir+"yolov3/yolov3.weights",gpu_id);
+        confidence_threshold = 0,65;
     }
 
-    void VehicleAnalyze::input(const string &filePath)
+    void VehicleAnalyze::input(const string &filepath)
     {
+        capture.open(filepath);
     }
 
-    void VehicleAnalyze::getTracks(Trackers &trackers)
+    void VehicleAnalyze::getTracks(vector<tracker::Tracker> &trackers)
     {
+        Mat frame;
+        int null_num = 0;
+        int skip_num = 0;
+
+        vector<ObjZoneDetect::Object> objs;
+        while(capture.read(frame))
+        {
+            if(null_num < numnull)
+            {
+                detector->Detect(frame,objs,confidence_threshold);
+                if (!objs.size())
+                {
+                    null_num++;
+                }
+            }
+            else if(skip_num < frameskip)
+            {
+                skip_num++;
+            }
+            else
+            {
+                skip_num = 0;
+                null_num = 0;
+            }
+
+            capindex++;
+
+        }
     }
-
-
-
 
     IVehicleAnalyze *CreateIVehicleAnalyze(const string &model_dir,const int gpu_id, const int frameskip, const int numnull)
     {
         IVehicleAnalyze *ptr=nullptr;
-        ptr = new VehicleAnalyze(model_dir,gpu_id,freamSkip,nullNull);
+        ptr = new VehicleAnalyze(model_dir,gpu_id,frameskip,numnull);
 
         return ptr;
-
-    }
-
-    void DestroyIVehicleAnalyze(IVehicleAnalyze *ptr)
-    {
-        delete ptr;
     }
 }
